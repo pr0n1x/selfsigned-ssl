bf3057ee60074513fcc595f2593e40e60114a946
pr0n1x <pr0n1x@yandex.ru>
2021-03-24 11:30:16 +0700

    Certificates moved to ./cert/ folder

---
+++ create_domain.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# @see https://habr.com/ru/post/352722/
+
+cwd=`dirname $0`
+cd $cwd
+
+if [ -z "$1" ]
+then
+	echo "Please supply a subdomain to create a certificate for";
+	echo "e.g. mysite.localhost"
+	exit;
+fi
+
+DOMAIN=$1
+COMMON_NAME=${2:-$1}
+CERT_FOLDER=cert
+
+if [ -f ${CERT_FOLDER}/${DOMAIN}.key ]; then
+	KEY_OPT="-key"
+else
+	KEY_OPT="-keyout"
+fi
+
+SUBJECT="/C=CA/ST=None/L=NB/O=None/CN=$COMMON_NAME"
+NUM_OF_DAYS=999
+
+echo "Create CSR... (CN: ${COMMON_NAME})"
+openssl req -new -newkey rsa:2048 -sha256 -nodes $KEY_OPT ${CERT_FOLDER}/${DOMAIN}.key -subj "$SUBJECT" -out ${CERT_FOLDER}/${DOMAIN}.csr
+
+echo "Cert Settings..."
+cat settings.ext | sed s/%%DOMAIN%%/${COMMON_NAME}/g > ${CERT_FOLDER}/${DOMAIN}.ssl.ext
+
+echo "Release Cert..."
+openssl x509 -req -in ${CERT_FOLDER}/${DOMAIN}.csr -CA ${CERT_FOLDER}/rootCA.pem -CAkey ${CERT_FOLDER}/rootCA.key -CAcreateserial -out ${CERT_FOLDER}/${DOMAIN}.crt -days $NUM_OF_DAYS -sha256 -extfile ${CERT_FOLDER}/${DOMAIN}.ssl.ext
+
+echo "Clear temp files..."
+rm ${CERT_FOLDER}/${DOMAIN}.ssl.ext
+rm ${CERT_FOLDER}/${DOMAIN}.csr
--- create_root_ca.sh
+++ create_root_ca.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
 # @see https://habr.com/ru/post/352722/
+# @see https://askubuntu.com/a/1159454
 
 cwd=`dirname $0`
 cd $cwd
 
-openssl genrsa -out rootCA.key 2048
-openssl req -x509 -new -nodes -key rootCA.key -sha256 -days 1024 -out rootCA.pem
+CERT_FOLDER=cert
+
+if [ -f "${CERT_FOLDER}/rootCA.key" ] \
+|| [ -f "${CERT_FOLDER}/rootCA.pem" ] \
+|| [ -f "${CERT_FOLDER}/rootCA.srl" ] ; then
+  echo "rootCA already created. You should to delete them before.";
+  exit 1;
+fi
+
+openssl genrsa -out ${CERT_FOLDER}/rootCA.key 2048
+openssl req -x509 -new -nodes -key ${CERT_FOLDER}/rootCA.key -sha256 -days 1024 -out ${CERT_FOLDER}/rootCA.pem
